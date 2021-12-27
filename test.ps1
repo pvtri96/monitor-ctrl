900cfc3b69ef4ed9373540f78a7b439aadfaa764
Tri Van Pham <tri.van.pham@mgm-tp.com>
2021-12-27 15:40:06 +0700

    add test.ps1

---
+++ test.ps1
@@ -0,0 +1,6 @@
+
+# ControlMyMonitor.exe /stab "" | GetNir.exe "Current Value" "VCPCode=10"
+# ControlMyMonitor.exe /GetValue "\\.\DISPLAY1\Monitor0" 10
+
+# ControlMyMonitor.exe /stab "" "\\.\DISPLAY2\Monitor0" | GetNir.exe "Current Value" "VCPCode=10" | Out-String
+
--- toPC.ps1
+++ toPC.ps1
@@ -3,9 +3,3 @@ ControlMyMonitor.exe /SetValue "\\.\DISPLAY1\Monitor0" 60 8
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.SendKeys]::SendWait("^%1")
 
-
-# ControlMyMonitor.exe /stab "" | GetNir.exe "Current Value" "VCPCode=10"
-# ControlMyMonitor.exe /GetValue "\\.\DISPLAY1\Monitor0" 10
-
-# ControlMyMonitor.exe /stab "" "\\.\DISPLAY2\Monitor0" | GetNir.exe "Current Value" "VCPCode=10" | Out-String
-
