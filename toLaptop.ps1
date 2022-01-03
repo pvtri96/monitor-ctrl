2958abf60d11d31cbe6e0ba51ca67e80c57416ca
Tri Van Pham <tri.van.pham@mgm-tp.com>
2022-01-03 10:12:09 +0700

    use monitor id

--- toLaptop.ps1
+++ toLaptop.ps1
@@ -1,4 +1,5 @@
-ControlMyMonitor.exe /SetValue "\\.\DISPLAY1\Monitor0" 60 5
+ControlMyMonitor.exe /SetValue "MONITOR\CMI3400\{4d36e96e-e325-11ce-bfc1-08002be10318}\0006" 60 7
 
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.SendKeys]::SendWait("^%2")
+
--- toPC.ps1
+++ toPC.ps1
@@ -1,4 +1,4 @@
-ControlMyMonitor.exe /SetValue "\\.\DISPLAY1\Monitor0" 60 8
+ControlMyMonitor.exe /SetValue "MONITOR\CMI3400\{4d36e96e-e325-11ce-bfc1-08002be10318}\0006" 60 8
 
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.SendKeys]::SendWait("^%1")
